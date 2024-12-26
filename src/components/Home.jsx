d9d9a0d011c8cc127d160aafa07064664946ea6b
DuyChinh <doanduychinh2102@gmail.com>
2024-12-26 10:53:56 +0700

    move img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,11 @@ export default function Home() {
     <div className="container">
       <header className="header">
         <div className="logo">
-            <img src="./src/assets/bmo.jpeg" alt="image"  className="img_logo"/>
+            <img src="./public/images/bmo_2.png" alt="image"  className="img_logo"/>
         </div>
         <div className="icons">
           <span>
-            <img src="./src/assets/icon_header.png" alt="" className="img_logo_2"/>
+            <img src="./public/images/icon_header.png" alt="" className="img_logo_2"/>
           </span>
           {/* <span>
             <IoIosHelpCircleOutline/>
